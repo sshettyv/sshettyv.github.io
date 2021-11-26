f18ec2c825f7962c09516fcdd95855895b35647e
sshettyv <vijethcshetty@gmail.com>
2021-11-26 10:01:50 +0200

    imges

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+function showImage(){
+let img = document.createElement("img");
+img.src="files/img1";
+let src = document.getElementsByClassName("image-gallery") 
+src.appendChild(img);
+ 
+ alert("ad");
+}
