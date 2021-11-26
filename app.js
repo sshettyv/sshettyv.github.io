ce9c98e8597ba0ac2ccc95d8b51918a38c2c5f9a
sshettyv <vijethcshetty@gmail.com>
2021-11-26 18:41:23 +0200

    b

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-function showImage(){
-let img = document.createElement("img");
-img.src="files/img1";
-let src = document.getElementsByClassName("image-gallery") 
-src.appendChild(img);
  
- alert("ad");
-}
