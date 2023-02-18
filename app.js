a2e3e44f1a42174f3a782a250f921b26a4abc9a3
vijeth shetty <vijethcshetty@gmail.com>
2023-02-18 18:41:30 +0200

    edited appjs removed gallery
    edited appjs removed gallery

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
- const gallery = document.getElementById('gallery');
- const home = document.getElementById('home');
- const popup = document.getElementById('popup');
- const bodyTag = document.getElementsByTagName('body');
- const selectedImage = document.getElementById('selectedImage');
- const imageIndexes= [1,2,3,4,5,6,7,8,9];
- const selectedIndex = null;
- 
- let w= window.innerWidth;
- let h= window.innerHeight;
- 
 
-
-
-
-
- imageIndexes.forEach((i) => {
-    const image = document.createElement('img');
-    image.src = `/files/img${i}.jpg`;
-    image.alt = `food image {i}`;
-    image.classList.add('galleryImg');
-    image.addEventListener('click',()=>{
-        popup.style.transform = 'translateY(0)';
-        selectedImage.src = `/files/img${i}.jpg`;
-        selectedImage.alt = `food image {i}`;
-    });
-    gallery.append(image);
-     
-
- })
-
- popup.addEventListener('click',()=>{
-    popup.style.transform = 'translateY(-100%)';
-    popup.src = '' ;
-    popup.alt='';
-
- })
 
  document.getElementById('githubLink').onclick = function(){
    window.open('https://github.com/sshettyv');
